0d1ab2a6d407c443c913999d80a72d63a29ebfeb
Yasmeen Hitti <39137511+YasmeenVH@users.noreply.github.com>
2026-09-08 11:45:35 -0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,17 +1,103 @@
+// function setup() {
+//     let canvas = createCanvas(windowWidth, windowHeight);
+//     canvas.parent("p5-container");
+// }
+
+// function draw() {
+//     clear();
+
+//     noFill();
+//     stroke(0);
+
+//     circle(mouseX, mouseY, 100);
+// }
+
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+// }
+
+let photo;
+let particles = [];
+
+function preload() {
+  photo = loadImage('assets/images/background.png');
+}
+
 function setup() {
-    let canvas = createCanvas(windowWidth, windowHeight);
-    canvas.parent("p5-container");
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
+
+  // Create the initial data particles
+  for (let i = 0; i < 150; i++) {
+    particles.push(new DataParticle());
+  }
 }
 
 function draw() {
-    clear();
+  background(0);
 
-    noFill();
-    stroke(0);
+  // Draw photograph
+  let scale = max(
+    width / photo.width,
+    height / photo.height
+  );
 
-    circle(mouseX, mouseY, 100);
+  let w = photo.width * scale;
+  let h = photo.height * scale;
+
+  image(photo, width / 2, height / 2, w, h);
+
+  // Update and draw data
+  for (let p of particles) {
+    p.update();
+    p.display();
+  }
+
+  // Lens
+  noFill();
+  stroke(255, 180);
+  strokeWeight(1);
+  circle(mouseX, mouseY, 180);
+}
+
+class DataParticle {
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.x = mouseX;
+    this.y = mouseY;
+
+    this.angle = random(TWO_PI);
+    this.speed = random(0.5, 2.5);
+
+    this.life = random(30, 120);
+
+    this.value = random(0, 99999)
+      .toFixed(random() > 0.5 ? 0 : 3);
+  }
+
+  update() {
+    this.x += cos(this.angle) * this.speed;
+    this.y += sin(this.angle) * this.speed;
+
+    this.life--;
+
+    if (this.life <= 0) {
+      this.reset();
+    }
+  }
+
+  display() {
+    fill(255, 180);
+    noStroke();
+
+    textSize(11);
+    text(this.value, this.x, this.y);
+  }
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
