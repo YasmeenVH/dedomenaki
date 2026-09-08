3e18abf709fae651e912a8512d7620e7a41fe867
Yasmeen Hitti <39137511+YasmeenVH@users.noreply.github.com>
2026-09-08 11:17:47 -0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -1,1 +1,17 @@
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent("p5-container");
+}
 
+function draw() {
+    clear();
+
+    noFill();
+    stroke(0);
+
+    circle(mouseX, mouseY, 100);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
